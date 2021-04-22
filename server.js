fbb1d2e7b09d56e1999e45b01d7fe8a2bebc5641
Devon <Dwill629@gmail.com>
2021-04-21 23:39:22 -0400

    updated server.js with require

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
+const burgers_controller = require("./controllers/burgers_controller");
