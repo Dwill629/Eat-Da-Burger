2731dd707a03a42d988fc62db035b29a2854d691
Devon <Dwill629@gmail.com>
2021-04-21 23:40:00 -0400

    added port to server

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
 const burgers_controller = require("./controllers/burgers_controller");
+
+const app = express();
+const PORT = process.env.PORT || 3306;
