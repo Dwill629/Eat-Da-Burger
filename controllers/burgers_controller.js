86547cf46ec2e30f6eed599afa8124bf747e97cd
Devon <Dwill629@gmail.com>
2021-04-21 23:53:31 -0400

    added router.get to controller js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,9 @@ const express = require("express");
 const burger = require("../models/burger");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.all(results =>
+    res.render("index", { burgers: results })
+  );
+});
