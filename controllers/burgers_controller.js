45f36d25c59bd60c8f45640ca990e2efab7a6bd4
Devon <Dwill629@gmail.com>
2021-04-21 23:54:45 -0400

    added router.put to controller js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,3 +17,13 @@ router.post("/api/burgers", (req, res) => {
     res.status(200).end();
   });
 });
+
+router.put("/api/burgers/:id", (req, res) => {
+  burger.update(req.body, req.params.id, results => {
+
+    if (results.affectedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
